5f29b546e6a41495f2613f283e3872e27769c8da
Erick <genghonghao@gmail.com>
2019-03-19 11:32:34 +0800

    bash 配置

---
+++ upcode/common.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#项目列表
+allow_project_list=(project1 project2 project3 project4)
+#gitserver
+gitserver="git.xxxxxxx.com"
+function _color_()
+{
+    case "$1" in
+        red)    nn="31";;
+        green)  nn="32";;
+        yellow) nn="33";;
+        blue)   nn="34";;
+        purple) nn="35";;
+        cyan)   nn="36";;
+    esac
+    ff=""
+    case "$2" in
+       bold)   ff=";1";;
+       bright) ff=";2";;
+       uscore) ff=";4";;
+       blink)  ff=";5";;
+       invert) ff=";7";;
+    esac
+    color_begin=`echo -e -n "\033[${nn}${ff}m"`
+    color_end=`echo -e -n "\033[0m"`
+    while read line; do
+        echo "${color_begin}${line}${color_end}"
+    done
+}
